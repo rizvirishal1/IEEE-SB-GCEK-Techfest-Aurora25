a5cd3578088c1878f78c403ec1370494d2dcc39e
rizvirishal1 <rizvirishal1@gmail.com>
2025-09-16 08:58:34 +0530

    feat: add cors

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-app.use(cors({ credentials: true }));
+const allowedOrigins = [
+    "http://localhost:5173",              // local dev
+    "https://aurora.ieeesbgcek.org",      // production
+    "https://www.aurora.ieeesbgcek.org"   // if www is also used
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 
