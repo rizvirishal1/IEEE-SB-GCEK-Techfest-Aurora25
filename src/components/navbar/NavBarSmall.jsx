1cf7ac1cf79b88650e667d5d71b777e1317b1593
tsn67 <tomsebastian67@gmail.com>
2025-09-30 08:25:29 +0000

    nav-bar-mobile-height-transition-fix

--- src/components/navbar/NavBarSmall.jsx
+++ src/components/navbar/NavBarSmall.jsx
@@ -3,7 +3,7 @@ import closeIcon from "../../assets/icons/close.svg";
 import menuIcon from "../../assets/icons/menu.svg";
 import { motion } from "framer-motion";
 
-const NavBarSmall = ({handleNavigation, getPath}) => {
+const NavBarSmall = ({ handleNavigation, getPath }) => {
 
     const container = `fixed top-[10px] w-[98%] z-50 rounded-[35px] backdrop-blur-[20px] bg-cyan-400/10 shadow-[0_4px_32px_0_rgba(31,38,135,0.37)] border border-white/18 mx-auto left-0 right-0 py-4 px-4 flex flex-col justify-center items-center`
     const header = `text-white font-nico text-4xl whitespace-nowrap`
@@ -31,22 +31,30 @@ const NavBarSmall = ({handleNavigation, getPath}) => {
                 <div className="py-3 pr-2">
                     <img onClick={() => setMenuOpen(!menuOpen)} className={iconStyle} src={menuOpen ? closeIcon : menuIcon}></img>
                 </div>
-
             </div>
 
 
             {menuOpen && (
-                <motion.div 
-                    initial={{scaleY: 0}}
-                    animate={{scaleY: 1}}
-                    transition={{ease: 'easeOut', duration: 0.24}}
-                    
-                    className="origin-top w-full h-fit justify-center mt-2 flex flex-col items-center gap-4 z-50">
+                <motion.div
+                    initial={{ height: 0, opacity: 1 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{ duration: 0.2, ease: "easeOut" }}
+                    className="overflow-hidden w-full mt-2 flex flex-col items-center gap-4 z-50"
+                >
                     {menu.map((item, i) => (
-                        <MenuItem handleNaviagtion={handleNavigation} setMenuOpen={setMenuOpen} path={paths[i]} getPath={getPath} name={item} key={`menu-item-s-${i}`} />
+                        <MenuItem
+                            handleNaviagtion={handleNavigation}
+                            setMenuOpen={setMenuOpen}
+                            path={paths[i]}
+                            getPath={getPath}
+                            name={item}
+                            key={`menu-item-s-${i}`}
+                        />
                     ))}
                 </motion.div>
             )}
+
         </div>
     )
 }
