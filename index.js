8e4ea3931b5bd3a6ebab07c745d2f1cd299b0e5f
rizvirishal1 <rizvirishal1@gmail.com>
2025-10-03 16:09:08 +0530

    up

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const allowedOrigins = [
     "http://localhost:3000",              // local dev - admin
     "https://aurora.ieeesbgcek.org",      // production
     "https://www.aurora.ieeesbgcek.org",   // if www is also used
-    "https://www.aurora.admin.ieeesbgcek.org/", // production - admin panel if www is also used
-    "https://aurora.admin.ieeesbgcek.org/"  // production - admin panel
+    "https://www.aurora.admin.ieeesbgcek.org", // production - admin panel if www is also used
+    "https://aurora.admin.ieeesbgcek.org"  // production - admin panel
 ];
 
 app.use(cors({
